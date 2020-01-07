0046e19b720c1a55c891f7afaa27630dc67d90cf
KingOfFlag <1435069316@qq.com>
2020-01-07 23:28:41 +0800

    上传 v0.2

--- excle.py
+++ excle.py
@@ -4,8 +4,16 @@ import string
 
 NumericList = ["int","short", "double", "float" ] # 数值列表
 IntegeList = ["int","short"] # 整型列表
-#区分数值类型
+typeRow = 3 # 定义变量类型的行
+propertysNameRow = 4 # 变量名所在的行
+dataStartRow = 5 # 数据开始的行
+exclePath = "./data.xlsx" # excle地址
+dataSheetName = "data" # 数据表名
+
 def DelimitationNumeric(value,typeName):
+    """
+    区分数值类型
+    """
     result = ""
     if typeName.value in NumericList:
         if typeName.value in IntegeList:
@@ -18,39 +26,47 @@ def DelimitationNumeric(value,typeName):
     else:
         result = '\"{v}\"'.format(v = value.value)
     return result
+ 
+def AnalysisPropertys(dataSheet):
+    """
+    解析变量名
+    """
+    result = []
+    for i in range(1,dataSheet.ncols):
+        propertyType = dataSheet.cell(typeRow-1, i)
+        propertysName = dataSheet.cell(propertysNameRow-1, i)
+        result.append((propertysName,propertyType))
+    return result
 
-excle = xlrd.open_workbook("./data.xlsx")
-dataSheet = excle.sheet_by_name("data")
+def GetPropertyName(propertyList,propertyIndex):
+    """
+    从列表中获取属性名
+    """
+    return propertyList[col - 1][0]
 
-print('nRow:{nrow},nCol:{ncol}'.format(nrow = dataSheet.nrows, ncol = dataSheet.ncols))
+def GetPropertyType(propertyList,propertyIndex):
+    """
+    从列表中获取属性类型
+    """
+    return propertyList[col - 1][1]
 
-typeRow = 3 # 定义变量类型的行
-propertysNameRow = 4 # 变量名所在的行
-dataStartRow = 5 # 数据开始的行
+if __name__ == '__main__':
+    excle = xlrd.open_workbook(exclePath)
+    dataSheet = excle.sheet_by_name(dataSheetName)
+    propertyList = AnalysisPropertys(dataSheet)
 
-propertyList = []
-propertysNameIndex = 0
-propertyTypeIndex = 1
-for i in range(1,dataSheet.ncols):
-    print(i,typeRow-1)
-    propertyType = dataSheet.cell(typeRow-1, i)
-    propertysName = dataSheet.cell(propertysNameRow-1, i)
-    propertyList.append((propertysName,propertyType))
-    pass
-
-result = '{'
-for row in range(dataStartRow - 1, dataSheet.nrows):
-    result += '{'
-    for col in range(1, dataSheet.ncols):
-        context = dataSheet.cell(row,col)
-        p = propertyList[col - 1][propertysNameIndex]
-        v = DelimitationNumeric(context,propertyList[col - 1][propertyTypeIndex])
-        result += '\"{Name}\":{value}'.format(Name = p.value, value = v)
-        if col != dataSheet.ncols -1:
-            result += ','
-    result += '}'
-    if row != dataSheet.nrows - 1:
-        result += ','
-    pass
-result += "}"
-print(result)
+    result = ''
+    for row in range(dataStartRow - 1, dataSheet.nrows):
+        result += '{'
+        for col in range(1, dataSheet.ncols):
+            context = dataSheet.cell(row,col)
+            p = GetPropertyName(propertyList,col - 1)
+            v = DelimitationNumeric(context,GetPropertyType())
+            result += '\"{Name}\":{value}'.format(Name = p.value, value = v)
+            if col != dataSheet.ncols -1:
+                result += ','
+        result += '}'
+        if row != dataSheet.nrows - 1:
+            result += '\n'
+        pass
+    print(result)
